7bf3f392a61b1573f39de96e10206e7c1c58241b
Minh Thu Tranová <tranomin@fel.cvut.cz>
2025-01-28 16:26:32 +0100

    Created project

---
+++ caterpillar/cmake-build-debug/CMakeFiles/caterpillar.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/caterpillar.dir/main.cpp.o"
+  "CMakeFiles/caterpillar.dir/main.cpp.o.d"
+  "caterpillar"
+  "caterpillar.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/caterpillar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
